1ee9d2e813b700f82f6768455527bb0e5d3a8c50
Saket Kumar <kumar.sak@northeastern.edu>
2022-10-31 13:27:07 -0400

    Fixed the routes for a7

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Nav from "../nav";
 import Assignment6 from "./a6";
+import Assignment7 from "./a7";
+import { Routes, Route } from "react-router";
 
 function Labs() {
   return (
     <div>
       <Nav />
-      <Assignment6 />
+      <Routes>
+        <Route index
+          element={<Assignment6 />} />
+        <Route path="a6" element={<Assignment6 />} />
+        <Route path="a7" element={<Assignment7 />} />
+      </Routes>
     </div>
   );
 }
+
 export default Labs;
+
