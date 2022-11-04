dc70662196efd4b8c84af999ce954c93ae036d44
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-03 21:07:41 -0400

    Added profile view completed

--- src/tuiter/profile/index.js
+++ src/tuiter/profile/index.js
@@ -18,11 +18,35 @@ const ProfileComponent = () => {
       </div>
       <div className="position-relative">
         <img className="img-fluid" src={`/images/${profileData.bannerPicture}`} alt="profile banner" />
-        <div className="position-absolute wd-dp-box">
+        <div className="position-absolute wd-dp-box ms-3">
           <img src={`/images/${profileData.profilePicture}`} className="wd-dp img-fluid" />
         </div>
-        <div className="d-flex justify-content-end pt-3">
+        <div className="d-flex justify-content-end pt-3 me-3">
           <Link to="/tuiter/edit-profile" type="button" className="btn btn-sm btn-outline-secondary rounded-pill">Edit Profile</Link>
+        </div>
+      </div>
+      <div className="ms-3 me-3">
+        <h6 className="mb-0 mt-4 fw-bold">{profileData.firstName} {profileData.lastName}</h6>
+        <p className="text-muted small m-0">@{profileData.handle}</p>
+        <p className="mt-3">{profileData.bio}</p>
+        <div className="mt-3 d-flex flex-row text-muted">
+          <span className="pe-3">
+            <i className="fa fa-location-dot" /> {profileData.location}
+          </span>
+          <span className="pe-3">
+            <i className="fa fa-cake-candles" /> Born: {profileData.dateOfBirth}
+          </span>
+          <span className="pe-3">
+            <i className="fa fa-calendar" /> Joined: {profileData.dateJoined}
+          </span>
+        </div>
+        <div className="mt-3 d-flex flex-row text-muted">
+          <span className="pe-3">
+            <b className="text-dark">{profileData.followingCount}</b> Following
+          </span>
+          <span className="pe-3">
+            <b className="text-dark">{profileData.followersCount}</b> Followers
+          </span>
         </div>
       </div>
     </div>
--- src/tuiter/profile/profile-reducer.js
+++ src/tuiter/profile/profile-reducer.js
@@ -3,10 +3,10 @@ import { createSlice } from "@reduxjs/toolkit";
 const currentUser = {
   firstName: 'Saket',
   lastName: 'Kumar',
-  handle: '@voonic',
+  handle: 'voonic',
   profilePicture: 'js.png',
   bannerPicture: 'banner.jpeg',
-  bio: 'Faculty, Software Engineer, AI, Space, and renewable enthusiast. Retuits and likes are not endorsements.',
+  bio: 'Student, Software Engineer, Game development, Space, and Coding enthusiast. Retuits and likes are not endorsements.',
   website: 'youtube.com/webdevtv',
   location: 'Boston, MA',
   dateOfBirth: '7/7/1968',
