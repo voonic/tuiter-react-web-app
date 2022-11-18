60998317d1b587ea3e8b255e73d46f328753b823
Saket Kumar <kumar.sak@northeastern.edu>
2022-11-17 20:52:25 -0500

    updated the tuits to read from posts

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,19 +1,18 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import PostSummaryItem
   from "./post-summary-item";
+import postsArray from './posts.json';
 
 const PostSummaryList = () => {
-  const postsArray = useSelector(state => state.tuits)
-  // return (
-  //   <ul className="list-group">
-  //     {
-  //       postsArray.map(post =>
-  //         <PostSummaryItem
-  //           key={post._id} post={post} />)
-  //     }
-  //   </ul>
-  // );
+  return (
+    <ul className="list-group">
+      {
+        postsArray.map(post =>
+          <PostSummaryItem
+            key={post._id} post={post} />)
+      }
+    </ul>
+  );
 };
 export default PostSummaryList;
 
